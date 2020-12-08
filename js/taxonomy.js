7192e750f1ea9c7d9894b8116354d232c367623a
rhotep <andreas.pittrich@posteo.de>
2020-12-08 23:34:13 +0100

    moved taxonomy

---
+++ js/taxonomy.js
@@ -0,0 +1,122 @@
+(function(exports){
+
+	exports.categories = [
+		{
+			name:		'information_counseling',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'law',
+			tags:		[],
+			colors:		['#f27020']
+
+		},
+
+		{
+			name:		'support',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'health',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'work',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'education',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'leisure',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'social',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'culture',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		{
+			name:		'living',
+			tags:		[],
+			colors:		['#f27020']
+		},
+
+		// {
+		// 	name:		'city',
+		// 	tags:		[],
+		// 	colors:		['#f27020']
+		// }
+	]
+
+	exports.types = [
+		{
+			name:		'location',
+			colors:		['#f27020', '#f27020'],
+		},
+		{
+			name:		'event',
+			colors:		['#7c51a2', '#7c51a2'],
+		},
+		{
+			name:		'service',
+			colors:		['#fec300', '#fec300'],
+		},
+		{
+			name:		'information',
+			colors:		['#62a7d5', '#62a7d5'],
+		}
+	]
+
+	exports.tags = {
+		targetGroups:[
+			'children',
+			'boys',
+			'girls',
+			'teenager',
+			'seniors',
+			'families',
+			'men',
+			'women',
+			'adults',
+			'lgbtiq',
+			'volunteers',
+		],
+		misc: [
+			'accessible',
+			'free',
+			'multi_language',
+			'no_status',
+			'volunteers_needed'
+		]
+	}
+
+
+
+
+
+}(
+	('undefined' !== typeof exports) 
+	? exports
+	: (this['ic'] = this['ic'] || {})['taxonomy'] = (this['ic']['taxonomy'] || {})
+))
